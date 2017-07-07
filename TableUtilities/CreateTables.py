8b0cc45f205f86c8256952a70abc75521257f551
Mattb1969 <matthew@bostintechnology.com>
2017-07-07 17:53:35 +0100

    Create Tables with sample data implemented

--- TableUtilities/CreateTables.py
+++ TableUtilities/CreateTables.py
@@ -34,7 +34,7 @@ MAX_TABLE_STATUS_CHANGE_TIME = 60
 
 ################################## FUNCTIONS ###################################
 
-def DynamedbConnection():
+def DynamodbConnection():
     """
     Connect to the dynamo db.
     setEndpoint is used to make a local connection rather than the remote connection
@@ -362,41 +362,40 @@ def CreateClientCountTable(db):
 ################################# DATA POPULATION #######################
 
 def WriteSensorValues(db, data, tstamp, device, sensor, acroynm, desc):
-        """
-        Update the SensorValues table with the given data and timestamp
-        Always using the same sensor
-        returns nothing
-        """
-        
-        #TODO: Needs to return a success / failure
+    """
+    Update the SensorValues table with the given data and timestamp
+    Always using the same sensor
+    returns nothing
+    """
+    
+    #TODO: Needs to return a success / failure
 
-        #TODO: Future upgrade is to capture the data if offline and send it when it reconnects.
-        
-        print ("device: %s, Timestamp: %s, Sensor: %s, Acroynm: %s, Desc: %s, Tag: %s" % (device, tstamp, sensor, acroynm, desc, data))
-        
-        return
-        try:
-            ans = db.put_item(
-                TableName='SensorValues',
-                Item={
-                    'Device_ID': {'N': str(device)},
-                    'TimeStamp': {'S': str(tstamp)},
-                    'Sensor_ID': {'N': str(sensor)},
-                    'SensorAcroynm': {'S' : str(acroynm)},
-                    'SensorDescription' : { 'S': str(desc)},
-                    'MVData': { 'M' : {
-                        'type': { 'S' : '1'},
-                        'value': { 'S' : str(data)}
-                        }},
-                    'Viewed': { 'BOOL' : False},
-                    },
-                )
-            # print("Create Item Response %s" % ans) #Debug
-        except:
-            print ("Unable to write data to AWS")
+    #TODO: Future upgrade is to capture the data if offline and send it when it reconnects.
+    
+    print ("device: %s, Timestamp: %s, Sensor: %s, Acroynm: %s, Desc: %s, Tag: %s" % (device, tstamp, sensor, acroynm, desc, data))
+    
+    try:
+        ans = db.put_item(
+            TableName='SensorValues',
+            Item={
+                'Device_ID': {'N': str(device)},
+                'TimeStamp': {'S': str(tstamp)},
+                'Sensor_ID': {'N': str(sensor)},
+                'SensorAcroynm': {'S' : str(acroynm)},
+                'SensorDescription' : { 'S': str(desc)},
+                'MVData': { 'M' : {
+                    'type': { 'S' : '1'},
+                    'value': { 'S' : str(data)}
+                    }},
+                'Viewed': { 'BOOL' : False},
+                },
+            )
+        # print("Create Item Response %s" % ans) #Debug
+    except Exception as exception:
+        print ("Unable to write data to AWS:%s " % exception)
 
 
-        return
+    return
         
 def WriteUsers(db, username, password, clientid, clientname, status, lastlogon, email, contact, creationdate, deviceid, acroynm, description):
         """
@@ -417,7 +416,7 @@ def WriteUsers(db, username, password, clientid, clientname, status, lastlogon, 
             ans = db.put_item(
                 TableName='Users',
                 Item={
-                    'UserName': {'N': str(username)},
+                    'UserName': {'S': str(username)},
                     'Password': {'S': str(password)},
                     'Client_ID': {'N': str(clientid)},
                     'ClientName': {'S' : str(clientname)},
@@ -427,23 +426,49 @@ def WriteUsers(db, username, password, clientid, clientname, status, lastlogon, 
                     'Contact' : { 'S' :str(contact)},
                     'CreationDate' : {'S' : str(creationdate)},
                     'Devices': { 'M' : {
-                        'DeviceID': { 'S' : str(ddeviceid)},
-                        'DeviceAcroynm': {'S' : str(dacroynm)},
+                        'DeviceID': { 'S' : str(deviceid)},
+                        'DeviceAcroynm': {'S' : str(acroynm)},
                         'DeviceDescription': { 'S' : str(description)}
                         }},
                     'Viewed': { 'BOOL' : False},
                     },
                 )
             # print("Create Item Response %s" % ans) #Debug
-        except:
-            print ("Unable to write data to AWS")
+        except Exception as exception:
+            print ("Unable to write data to AWS: %s" % exception)
 
 
         return
-        
+
+def WriteClientCount(db, count):
+    """
+    Update the ClientCount table with the given data
+    returns nothing
+    """
+    
+    #TODO: Needs to return a success / failure
+
+    #TODO: Future upgrade is to capture the data if offline and send it when it reconnects.
+    
+    print ("Client Count: %s" % count)
+    
+    try:
+        ans = db.put_item(
+            TableName='ClientCount',
+            Item={
+                'CC_ID': {'N': '1'},
+                'QtyClients': {'S': str(count)},
+                },
+            )
+        # print("Create Item Response %s" % ans) #Debug
+    except Exception as exception:
+        print ("Unable to write data to AWS:%s " % exception)
+
+
+    return
 ################################## MAIN ########################################
 
-conn = DynamedbConnection()
+conn = DynamodbConnection()
 
 """
 v 1 table setup
@@ -461,6 +486,27 @@ ClientCount = CreateClientCountTable(conn)
 SensorValues = CreateSensorValuesTable(conn)
 
 #Populate some sample data
+# WriteUsers(db, username, password, clientid, clientname, status, lastlogon, email, contact, 
+#                   creationdate, deviceid, acroynm, description):
+print("Write Users\n***********")
+
+WriteUsers(conn, "m@mlb.com", "password", 1, "BostinTech", 'ACTIVE', '05-05-2017 15:05:34', 'm@mlb.com', '07676 543322', 
+                    '07-07-2017 16:05:34', 1, 'GrnHse1', 'Greenhouse 1')
+WriteUsers(conn, "l@mlb.com", "pssaword", 2, "BostinTech", 'ACTIVE', '06-06-2017 16:05:34', 'l@mlb.com', '06677 543322', 
+                    '07-07-2017 16:05:34', 2, 'Shed1', 'Mushroom Shed 1')
+WriteUsers(conn, "c@mlb.com", "passowrd", 3, "BostinTech", 'ACTIVE', '07-07-2017 17:05:34', 'c@mlb.com', '05566 543322', 
+                    '07-07-2017 16:05:34', 3, 'Cons', 'conservatory')
+WriteClientCount(conn, 3)
+print("Write Sensor Values\n*******************")
+# WriteSensorValues(db, data, tstamp, device, sensor, acroynm, desc)
+WriteSensorValues(conn, '26.4 Deg C', '07-07-2017 :05:05:34.000', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '26.5 Deg C', '07-07-2017 02:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '26.6 Deg C', '07-07-2017 04:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '23.4 Deg C', '07-07-2017 06:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '18.7 Deg C', '07-07-2017 08:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '16.5 Deg C', '07-07-2017 10:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+WriteSensorValues(conn, '14.5 Deg C', '07-07-2017 12:05:34', 1, 1, 'Temp1', 'Temperature Sensor 1')
+
 
 
 print ("Completed")
