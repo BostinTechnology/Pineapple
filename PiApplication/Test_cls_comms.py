b4ef127607eb2d026157f9c435a7694cbfa5d443
Mattb1969 <matthew@bostintechnology.com>
2017-08-26 21:37:13 +0100

    comms unittesting in progress

--- PiApplication/Test_cls_comms.py
+++ PiApplication/Test_cls_comms.py
@@ -9,6 +9,7 @@ This software performs the unit testing of the cls_comms - i2c_comms code
 import unittest
 from unittest.mock import patch
 from unittest.mock import MagicMock
+from unittest.mock import call
 import importlib
 import logging
 import logging.config
@@ -17,69 +18,222 @@ import dict_LoggingSetup
 #BUG: If this program is in the test directory, it doesn't find this import
 from cls_comms import i2c_comms
 
+@patch('smbus.SMBus')
 class Testi2cCommsInit(unittest.TestCase):
     """
     Test the i2c comms module
     - creates the smbus object
     - stops if it can't create smbus
-    
+
     """
-    @patch('smbus.SMBus')
     def test_init(self, mock_smbus):
         """
         test the instationisation of the module
         """
+        gbl_log.debug("[TEST] test_init")
         # need to mock _open_port
         result = i2c_comms()            # returns the object i2c_comms
-        
+
         self.assertIsInstance(result, object)
-    
-    @patch('smbus.SMBus')
+
     def test_init_fails(self, mock_smbus):
         """
         test the instationisation of the module
         """
+        gbl_log.debug("[TEST] test_init_fails")
         mock_smbus.side_effect = Exception('FileNotFoundError')
-        
+
         with self.assertRaises(SystemExit):
             result = i2c_comms()            # returns the object i2c_comms
-    
+
+class Testi2cCommsRepeatedStart(unittest.TestCase):
+    """
+    Test the repeated start is set correctly.
+    """
+    def test_repeated_start(self):
+        """
+        Check the repeated start setting is correct
+        """
+        gbl_log.debug("[TEST] test_repeated_start")
+        result = False
+        self.assertTrue(result)
+
+@patch('smbus.SMBus')
 class Testi2cComms_read_byte(unittest.TestCase):
     """
     Test the i2c comms module to read a byte
     - address and byte valid
-    - invalid byte
-    - invalid address
-    - invalid byte and address
     - exception thrown
-    
+
     Checking
     - address and byte values correctly passed in
     """
-    @patch('smbus.SMBus')
-    def Setup(self):
-        """
-        Setup a temporary connection for the smbus
-        """
-        self.comms = i2c_comms()
-        return
-        
-    def test_read_addr_byte_valid(self):
+
+    def test_read_addr_byte_valid(self, mock_smbus):
         """
         Test the routine with a valid address and byte values
         """
-        
-        read_data_byte(sens_addr, byte_addr)
+        gbl_log.debug("[TEST] test_read_addr_byte_valid")
+        mock_smbus().read_byte_data.return_value = 0x45
 
-    
-        
+        comms = i2c_comms()
+        result = comms.read_data_byte(0x60, 0x10)
+        mock_smbus().read_byte_data.assert_called_with(0x60, 0x10)
+        self.assertEqual(result, 0x45)
+
+    def test_read_addr_byte_exception(self, mock_smbus):
+        """
+        Test the routine with an exception raised
+        """
+        #BUG: Need to check the exception raised
+
+        gbl_log.debug("[TEST] test_read_addr_byte_exception")
+        mock_smbus().read_byte_data.side_effect = Exception('IOError')
+
+        comms = i2c_comms()
+        result = comms.read_data_byte(0x50, 0x16)
+        mock_smbus().read_byte_data.assert_called_with(0x50, 0x16)
+
+        self.assertEqual(result, 0)
+
+@patch('smbus.SMBus')
+class Testi2cComms_read_data_bytes(unittest.TestCase):
+    """
+    Test the i2c comms module to read a range of bytes
+    - address and bytes are valid
+    - returns empty list if any of the replies are invalid
+    - handles exception
+    parameters(sens_addr, start_byte, no_bytes)
+    """
+
+    def test_read_addr_bytes_valid(self, mock_smbus):
+        """
+        Test the routine with a valid range of bytes
+        Request a range and check
+        """
+        gbl_log.debug("[TEST] test_read_addr_bytes_valid")
+        mock_smbus().read_byte_data.side_effect = [0x26, 0x27, 0x28]
+        calls = [call(0x45, 0x12),call(0x45, 0x13),call(0x45, 0x14)]
+        comms = i2c_comms()
+        result = comms.read_data_bytes(0x45, 0x12, 3)
+        mock_smbus().read_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, [0x26, 0x27, 0x28])
+
+    def test_read_addr_bytes_exception(self, mock_smbus):
+        """
+        Test the routine with an exception raised whilst reading each address and byte values
+        """
+        #BUG: Need to check the exception raised
+
+        gbl_log.debug("[TEST] test_read_addr_bytes_exception")
+        mock_smbus().read_byte_data.side_effect = [Exception('IOError'), 0x26, 0x27]
+        calls = [call(0x45, 0x12),call(0x45, 0x13),call(0x45, 0x14)]
+        comms = i2c_comms()
+        result = comms.read_data_bytes(0x45, 0x12, 3)
+        mock_smbus().read_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, [])
+
+        mock_smbus().read_byte_data.side_effect = [0x26, Exception('IOError'), 0x27]
+        calls = [call(0x45, 0x12),call(0x45, 0x13),call(0x45, 0x14)]
+        comms = i2c_comms()
+        result = comms.read_data_bytes(0x45, 0x12, 3)
+        mock_smbus().read_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, [])
+
+        mock_smbus().read_byte_data.side_effect = [0x26, 0x27, Exception('IOError')]
+        calls = [call(0x45, 0x12),call(0x45, 0x13),call(0x45, 0x14)]
+        comms = i2c_comms()
+        result = comms.read_data_bytes(0x45, 0x12, 3)
+        mock_smbus().read_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, [])
+
+@patch('smbus.SMBus')
+class Testi2cComms_write_byte(unittest.TestCase):
+    """
+    Test the i2c comms module to write a byte
+    - address and byte and value valid
+    - exception thrown
+
+    Checking
+    - address and byte values correctly passed in
+    """
+
+    def test_write_addr_byte_valid(self, mock_smbus):
+        """
+        Test the routine with a valid address and byte values
+        """
+        gbl_log.debug("[TEST] test_write_addr_byte_valid")
+        mock_smbus().write_byte.return_value = True
+
+        comms = i2c_comms()
+        result = comms.write_data_byte(0x60, 0x10, 0x80)
+        mock_smbus().write_byte_data.assert_called_with(0x60, 0x10, 0x80)
+        self.assertEqual(result, True)
+
+    def test_write_addr_byte_exception(self, mock_smbus):
+        """
+        Test the routine with an exception raised
+        """
+        #BUG: Need to check the exception raised
+
+        gbl_log.debug("[TEST] test_write_addr_byte_exception")
+        mock_smbus().write_byte.side_effect = Exception('IOError')
+
+        comms = i2c_comms()
+        result = comms.write_data_byte(0x50, 0x16, 0xff)
+        mock_smbus().write_byte_data.assert_called_with(0x50, 0x16, 0xff)
+
+        self.assertEqual(result, True)
+
+@patch('smbus.SMBus')
+class Testi2cComms_write_data_bytes(unittest.TestCase):
+    """
+    Test the i2c comms module to write a range of bytes
+    - address and bytes and values are valid
+    - handles exception
+    """
+
+    def test_write_addr_bytes_valid(self, mock_smbus):
+        """
+        Test the routine with a valid range of bytes
+        Request a range and check
+        """
+        gbl_log.debug("[TEST] test_write_addr_bytes_valid")
+        mock_smbus().write_byte_data.side_effect = [True, True, True]
+        calls = [call(0x45, 0x12, 0x20),call(0x45, 0x13, 0x21),call(0x45, 0x14, 0x22)]
+        comms = i2c_comms()
+        result = comms.write_data_bytes(0x45, 0x12, [0x20, 0x21, 0x22])
+        mock_smbus().write_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, True)
+
+    def test_write_addr_bytes_exception(self, mock_smbus):
+        """
+        Test the routine with an exception raised whilst writing each address and byte values
+        """
+        #BUG: Need to check the exception raised
+        gbl_log.debug("[TEST] test_write_addr_bytes_exception")
+        mock_smbus().write_byte_data.side_effect = Exception('IOError')
+        comms = i2c_comms()
+        result = comms.write_data_bytes(0x45, 0x12, [0x33, 0x34, 0x35])
+        mock_smbus().write_byte_data.assert_called_with(0x45, 0x12, 0x33)
+        self.assertEqual(result, False)
+
+        mock_smbus().write_byte_data.side_effect = [True, Exception('IOError')]
+        calls = [call(0x45, 0x12, 0x33),call(0x45, 0x13, 0x34)]
+        comms = i2c_comms()
+        result = comms.write_data_bytes(0x45, 0x12, [0x33, 0x34, 0x35])
+        mock_smbus().write_byte_data.assert_has_calls(calls)
+        self.assertEqual(result, False)
+
+
+
 def SetupLogging():
     """
     Setup the logging defaults
     Using the logger function to span multiple files.
     """
     global gbl_log
-    
+
     log_cfg = dict(
         version = 1,
         formatters = {
@@ -105,13 +259,13 @@ def SetupLogging():
     gbl_log = logging.getLogger()
 
     gbl_log.info("File Logging Started, current level is %s" % gbl_log.getEffectiveLevel)
-    
+
     return
 
 if __name__ == '__main__':
     SetupLogging()
-    
+
     gbl_log.critical("\n\n     [TEST] i2c test comms started\n\n")
-    
+
     unittest.main()
 
--- PiApplication/cls_comms.py
+++ PiApplication/cls_comms.py
@@ -27,11 +27,11 @@ class i2c_comms():
     """
     This routine will setup and control the low level comms out to the various
     protocols
-    
+
     By default, it just creates an instance of itself and opens the port
 
     """
-    
+
     def __init__(self):
         """
         Setup ready for comms, opening the requried port
@@ -41,14 +41,14 @@ class i2c_comms():
 
         self.log.debug("[I2C COMMS] Class initiated")
 
-            
+
         return
-    
+
     def transcieve_data(self, data):
         """
         Send and receive data
         """
-        
+
         return
 
     def send_data(self, data):
@@ -56,7 +56,7 @@ class i2c_comms():
         Send data on the chosen connection
         """
         return
-        
+
     def read_data_byte(self, sens_addr, byte_addr):
         """
         Read a data byte on the chosen connection
@@ -64,7 +64,7 @@ class i2c_comms():
 
         reply = self._read_byte(sens_addr, byte_addr)
         return reply
-    
+
     def read_data_bytes(self, sens_addr, start_byte, no_bytes):
         """
         Read a quantity of bytes, starting from start byte
@@ -72,11 +72,12 @@ class i2c_comms():
             0x10, 0x11, 0x12, 0x13, 0x14
         returns a list of values or an empty string if error occurred
         """
-        self.log.info("[I2C COMMS] Read Data Bytes from address %s starting at %s for %s bytes" 
+        self.log.info("[I2C COMMS] Read Data Bytes from address %s starting at %s for %s bytes"
                         % (sens_addr, start_byte, no_bytes))
         response = []
         for byte in range(start_byte, start_byte+no_bytes):
             reply = self._read_byte(sens_addr, byte)
+            #BUG: reply will return 0 if error occurs, not an empty string
             if reply != '':
                 response.append(reply)
             else:
@@ -93,7 +94,7 @@ class i2c_comms():
             0x10=0x34, 0x11=0x45, 0x12=0x56
         returns confirmation
         """
-        self.log.info("[I2C COMMS] Write Data Bytes to address %s starting at %s with bytes %s" 
+        self.log.info("[I2C COMMS] Write Data Bytes to address %s starting at %s with bytes %s"
                         % (sens_addr, start_byte, data_bytes))
         response = False
         addr = start_byte
@@ -105,18 +106,18 @@ class i2c_comms():
             addr = addr + 1
             time.sleep(I2C_WRITE_DELAY)
         return response
-            
+
     def write_data_byte(self, sens_addr, addr_byte, data_byte):
         """
         Write a byte to the address byte
         So passing (0x50, 0x10, 0x34) will write 0x34 into address byte 0x10
         returns confirmation
         """
-        self.log.info("[I2C COMMS] Write Data Byte to address %s byte %s with value %s" 
+        self.log.info("[I2C COMMS] Write Data Byte to address %s byte %s with value %s"
                         % (sens_addr, addr_byte, data_byte))
         response = self._write_byte(sens_addr, addr_byte, data_byte)
         return response
-    
+
     def repeated_start(self, repeated=True):
         """
         This function sets the I2C bus to use Repeated Start Mode
@@ -145,7 +146,7 @@ class i2c_comms():
 #    P R I V A T E   F U N C T I O N S
 #
 #-----------------------------------------------------------------------
-        
+
     def _open_port(self):
         """
         Open the i2c comms port
@@ -160,7 +161,7 @@ class i2c_comms():
             sys.exit()
         self.log.info("[I2C COMMS] I2C connection created:%s" % self.connection)
         return
-    
+
     def _read_byte(self, addr, byte_no):
         """
         Read a byte from the given address, returns either the value read or an empty binary string
@@ -173,10 +174,10 @@ class i2c_comms():
                 %(byte_no, addr, value))
             self.log.exception("[I2C COMMS] _read_byte Exception Data")
             value = 0
-        self.log.debug("[I2C COMMS] Read Byte from address:%s from i2c device:%s and got this response:%s" 
+        self.log.debug("[I2C COMMS] Read Byte from address:%s from i2c device:%s and got this response:%s"
                         % (byte_no, addr, value))
         return value
-    
+
     def _write_byte(self, addr, byte_no, value):
         """
         Write a byte from the given address.
@@ -186,28 +187,28 @@ class i2c_comms():
         try:
             self.connection.write_byte_data(addr, byte_no, value)
         except:
-            self.log.warning("[I2C COMMS] Unable to write byte:%s of value:%s from i2c device:%s"
+            self.log.warning("[I2C COMMS] Unable to write byte:%s with value:%s to i2c device:%s"
                 %(byte_no, value, addr))
             self.log.exception("[I2C COMMS] _write_byte Exception Data")
         else:
-            self.log.info("[I2C COMMS] Write to Byte:%s of value:%s for i2c device:%s" 
+            self.log.info("[I2C COMMS] Write to Byte:%s of value:%s for i2c device:%s"
                         % (byte_no, value, addr))
             response = True
-        return response    
+        return response
 
 
 class SPi_comms():
     """
     This routine will setup and control the low level comms out to the various
     protocols
-    
+
     By default, it just creates an instance of itself and opens the port
-    
+
     It is required to be given the port to work with, I2C, SPI, SERIAL
     - port is the type of connection I2C, SPI or SERIAL
     - address is the address of the device, default to 0x00 if not required
     """
-    
+
     def __init__(self, address=b'\x00'):
         """
         Setup ready for comms, opening the requried port
@@ -216,14 +217,14 @@ class SPi_comms():
 
         self.sensor_address = address
         self._open_port()
-            
+
         return
-    
+
     def transcieve_data(self, data):
         """
         Send and receive data
         """
-        
+
         return
 
     def send_data(self, data):
@@ -231,7 +232,7 @@ class SPi_comms():
         Send data on the chosen connection
         """
         return
-        
+
     def read_data_byte(self, byte_addr):
         """
         Read a data byte on the chosen connection
@@ -248,7 +249,7 @@ class SPi_comms():
 #
 #-----------------------------------------------------------------------
 
-    
+
     def _open_port(self):
         """
         Open the spi comms port
@@ -257,23 +258,23 @@ class SPi_comms():
         self.log.critical("[COMMS] SPI comms is currently not supported, pleae contact support")
         print("\nError Occurred, program halted - contact support\n")
         sys.exit()
-        
+
         #self.connection = ????
         return
-    
+
 
 class Serial_comms():
     """
     This routine will setup and control the low level comms out to the various
     protocols
-    
+
     By default, it just creates an instance of itself and opens the port
-    
+
     It is required to be given the port to work with, I2C, SPI, SERIAL
     - port is the type of connection I2C, SPI or SERIAL
     - address is the address of the device, default to 0x00 if not required
     """
-    
+
     def __init__(self, address=b'\x00'):
         """
         Setup ready for comms, opening the requried port
@@ -281,14 +282,14 @@ class Serial_comms():
         self.log = logging.getLogger()
         self.sensor_address = address
         self._open_port()
-            
+
         return
-    
+
     def transcieve_data(self, data):
         """
         Send and receive data
         """
-        
+
         return
 
     def send_data(self, data):
@@ -296,7 +297,7 @@ class Serial_comms():
         Send data on the chosen connection
         """
         return
-        
+
     def read_data_byte(self, byte_addr):
         """
         Read a data byte on the chosen connection
@@ -311,7 +312,7 @@ class Serial_comms():
 #    P R I V A T E   F U N C T I O N S
 #
 #-----------------------------------------------------------------------
-        
+
     def _open_port(self):
         """
         Open the serial comms port
@@ -320,10 +321,10 @@ class Serial_comms():
         self.log.critical("[COMMS] Serial comms is currently not supported, pleae contact support")
         print("\nError Occurred, program halted - contact support\n")
         sys.exit()
-        
+
         #self.connection = ????
         return
-        
+
 
 #-----------------------------------------------------------------------
 #
@@ -335,13 +336,13 @@ class Serial_comms():
 def selftest():
     """
     Validate the class is working
-    
+
     """
     return
 
 if __name__ == '__main__':
     # setup logging
 
-    
+
     selftest()
 
